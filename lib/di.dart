a4e323575db4610969486e6cda0c7a4e11e4e84e
s0ngkran <jinnawat8@gmail.com>
2022-05-06 01:45:36 +0700

    get storage

--- lib/di.dart
+++ lib/di.dart
@@ -2,6 +2,11 @@ import 'package:get/get.dart';
 
 import 'shared/services/services.dart';
 
+// class DenpendencyInjection {
+//   static Future<void> init() async {
+//     await Get.putAsync(() => StorageService().init());
+//   }
+// }
 class DenpendencyInjection {
   static Future<void> init() async {
     await Get.putAsync(() => StorageService().init());
--- lib/shared/services/storage_service.dart
+++ lib/shared/services/storage_service.dart
@@ -1,8 +1,15 @@
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class StorageService extends GetxService {
-  Future<SharedPreferences> init() async {
-    return await SharedPreferences.getInstance();
+  Future<bool> init() async {
+    // return await SharedPreferences.getInstance();
+    var res = await GetStorage.init();
+    var box = Get.put(GetStorage());
+
+    print('init GetStorage res=$res');
+    print('box --$box');
+    return res;
   }
 }
